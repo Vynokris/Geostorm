d349a92667e80acfda99fb37e6b486f41385d993
Rémi SERRA <r.serra@student.isartdigital.com>
2022-03-11 15:21:07 +0100

    Added a spawning animation to enemies.

--- Geostorm/Core/Enemy.cs
+++ Geostorm/Core/Enemy.cs
@@ -1,22 +1,21 @@
 ﻿using System.Numerics;
 using static MyMathLib.Geometry2D;
 using Geostorm.GameData;
+using Geostorm.Utility;
 
 namespace Geostorm.Core
 {
     public abstract class Enemy : Entity
     {
-        protected int SpawnDelay = 0;
+        public readonly Cooldown SpawnDelay = new(0);
 
         public Enemy() { }
-        public Enemy(Vector2 pos, float health, int spawnDelay) : base(pos, Vector2Create(3, 0), 0, health) { SpawnDelay = spawnDelay; }
+        public Enemy(Vector2 pos, float health, int spawnDelay) : base(pos, Vector2Create(3, 0), 0, health) { SpawnDelay.ChangeDuration(spawnDelay); }
 
         public sealed override void Update(in GameState gameState, in GameInputs gameInputs, ref GameEvents gameEvents)
         {
-            if (SpawnDelay <= 0)
+            if (SpawnDelay.Update())
                 DoUpdate(gameState, gameInputs, ref gameEvents);
-            else
-                SpawnDelay--;
         }
 
         public abstract void DoUpdate(in GameState gameState, in GameInputs gameInputs, ref GameEvents gameEvents);
